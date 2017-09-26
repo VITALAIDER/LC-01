b4d7fd6ea8d4b8dd0c56d9437eb81a6b0167e011
Vitalie Serdesniuc <vitalie.serdesniuc@expertnetwork.ro>
2017-09-26 07:41:41 +0300

    Initial commit

---
+++ Models/Professor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace LC_01.Models
+{
+    public class Professor
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public List<Student> Students  { get; set; }
+}
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public List<Project> Projects { get; set; } 
+    }
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public Student Student { get; set; }
+    }
+
+    public class ProjectContect : DbContext
+    {
+        public DbSet<Professor> Professors { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LC_01.Startup))]
+namespace LC_01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
